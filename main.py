ba8f7cbe07d605748d0aaa31471af520ccbb49c7
RenanLeviathan <renan.leviathan@gmail.com>
2017-10-31 16:28:54 -0300

    mudanças

--- main.py
+++ main.py
@@ -4,35 +4,41 @@ Created on Tue Oct 17 16:09:36 2017
 @author: Israël e Renan
 """
 class Grafo:
-	def __init__(self,vertices,inicial):
-		self.marc=[]
-		self.lista={}
-		self.marc.append(inicial)
-		for v in vertices:
-			w=[x for x in input("Adjacentes a {}:\n".format(v)).split()]
-			self.lista[v]=w
-			
-	def mostrar(self):
-		print(self.lista)
-	#explora as arestas do grafo	
-	def explorar(self):
-		exp=[]
-		comecou=False
-		inicial=self.marc[0]
-		for v in self.lista.keys():
-			if v != inicial and not comecou: continue
-			else: comecou=True
-			if v not in self.marc: self.marc.append(v)
-			for w in self.lista[v]:
-				if w not in self.marc: self.marc.append(w)
-				if (w in self.lista[v]) and (((v,w) not in exp) and ((w,v) not in exp)):
-					exp.append((v,w))
-					v=w
-		return exp
-   
-vertices=[x for x in input("Vértices do grafo:\n").split()]
+  def __init__(self,grafo,inicial):
+    self.marc=[]
+    self.lista={}
+    self.lista=grafo
+    self.marc.append(inicial)
+              
+  def mostrar(self):
+    print(self.lista)
+#explora as arestas do grafo	
+  def explorar(self):
+    exp=[]
+    comecou=False
+    inicial=self.marc[0]
+    for v in self.lista.keys():
+      if v != inicial and not comecou: continue
+      else: comecou=True
+      if v not in self.marc: self.marc.append(v)
+      for w in self.lista[v]:
+        if w not in self.marc: self.marc.append(w)
+        else: print("Aresta de retorno: ",(v,w))
+        if (w in self.lista[v]) and (((v,w) not in exp) and ((w,v) not in exp)):
+          exp.append((v,w))
+          v=w
+    return exp
+
+grafo={
+  'a':['b','c'],
+  'b':['a','c','d','f'],
+  'c':['a','b','d','e'],
+  'd':['b','c','e','f'],
+  'e':['c','d','f'],
+  'f':['b','d','e']
+  }
 ini=input("Vértice inicial da busca:\n")
-g=Grafo(vertices,ini)
+g=Grafo(grafo,ini)
+print("Arestas de busca:\n")
 exp=g.explorar()
-g.mostrar()
 print([i for i in exp])
