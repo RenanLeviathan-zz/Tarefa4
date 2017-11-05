253ef6c7fb9ad5df0d15f260a93734b9efc29cac
RenanLeviathan <renan.leviathan@gmail.com>
2017-11-04 21:42:34 -0300

    #visualisation de données

---
+++ Graph.py
@@ -0,0 +1,28 @@
+class Grafo:
+  def __init__(self,grafo):
+    self.marc=[]
+    self.lista={}
+    self.lista=grafo
+    self.exp=[]
+    self.history=""
+              
+  def get(self):
+    return self.lista
+    
+  def arestas_busca(self):
+    return self.exp
+  def get_history(self):
+    return self.history
+#explora as arestas do grafo    
+  def explorar(self,v):
+    self.marc.append(v)
+    for w in self.lista[v]:
+      if w not in self.marc:
+        self.history+="Aresta de busca: ({},{})\n".format(v,w)
+        self.exp.append((v,w))
+        self.marc.append(w)
+        self.explorar(w)
+      else:
+        if ((v,w) not in self.exp) and ((w,v) not in self.exp):
+          self.history+="Aresta de retorno: ({},{})\n".format(v,w)
+          self.exp.append((v,w))
--- main.py
+++ main.py
@@ -4,33 +4,39 @@ Created on Tue Oct 17 16:09:36 2017
 Busca em profundidade
 @author: Israël e Renan
 """
-class Grafo:
-  def __init__(self,grafo,inicial):
-    self.marc=[]
-    self.lista={}
-    self.lista=grafo
-    self.marc.append(inicial)
-              
-  def mostrar(self):
-    print(self.lista)
-#explora as arestas do grafo	
-  def explorar(self):
-    exp=[]
-    comecou=False
-    inicial=self.marc[0]
-    for v in self.lista.keys():
-      if v != inicial and not comecou: continue
-      else: comecou=True
-      if v not in self.marc: self.marc.append(v)
-      for w in self.lista[v]:
-        if w not in self.marc: self.marc.append(w)
-        else: print("Aresta de retorno: ",(v,w))
-        if (w in self.lista[v]) and (((v,w) not in exp) and ((w,v) not in exp)):
-          print("Aresta de busca: ",(v,w))
-          exp.append((v,w))
-          v=w
-    return exp
-
+from Graph import *
+from tkinter import *
+from random import *
+class Main:
+  def __init__(self,master=None,graph=None):
+    self.widget1 = Frame(master,width=500,height=500)
+    self.graph=Grafo(graph)
+    self.widget1.pack()
+    self.canvas=Canvas(self.widget1)
+    self.canvas.pack()
+    ini=input("Vértice inicial da busca:\n")
+    self.graph.explorar(ini)
+    seed(7)
+    plotados=[]
+    #plota os vértices do grafo
+    for l in self.graph.get():
+      sort=False
+      while not sort:
+        x=randint(10,200)
+        y=randint(10,200)
+        if (x,y) not in plotados:
+          plotados.append((x,y))
+          sort=True
+      self.canvas.create_oval(x-10,y-10,x+10,y+10,fill="red")
+      self.canvas.create_text(x,y,text=l)
+      #desenha as arestas
+    for i in range(1,len(plotados)):
+      self.canvas.create_line(plotados[i-1][0],plotados[i-1][1],plotados[i][1],plotados[i][1])
+    self.msg=Label(self.widget1,text=self.graph.get_history())
+    self.msg.pack()
+         
+    
+        
 grafo={
   'a':['b','c'],
   'b':['a','c','d','f'],
@@ -39,8 +45,7 @@ grafo={
   'e':['c','d','f'],
   'f':['b','d','e']
   }
-ini=input("Vértice inicial da busca:\n")
-g=Grafo(grafo,ini)
-exp=g.explorar()
-print("Arestas de busca:\n")
-print([i for i in exp])
+#visualização do grafo
+root=Tk()
+Main(root,grafo)
+root.mainloop()
